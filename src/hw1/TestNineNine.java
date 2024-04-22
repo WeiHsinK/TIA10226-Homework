5c312cca6ce0f487b722fc26408f33737eb329ba
Kuei <a121212106@gmail.com>
2024-04-23 01:26:46 +0800

    First commit

---
+++ src/hw1/TestNineNine.java
@@ -0,0 +1,46 @@
+package hw1;
+
+//輸出九九乘法表
+public class TestNineNine {
+	public static void main(String[] args) {
+		// 使用for迴圈+while迴圈
+		System.out.println("使用for迴圈+while迴圈製作九九乘法表:");
+		System.out.println();
+		for (int i = 1; i <= 9; i++) {
+			int a = 1;
+			while (a <= 9) {
+				System.out.print(i + "*" + a + "=" + i * a + "\t");
+				a++;
+			}
+			System.out.println();
+		}
+
+		System.out.println("----------------------------------");
+		// 使用for迴圈+do while迴圈
+		System.out.println("使用for迴圈+do while迴圈製作九九乘法表:");
+		for (int i = 1; i <= 9; i++) {
+			int a = 1;
+			System.out.println();
+			do {
+				System.out.print(i + "*" + a + "=" + i * a + "\t");
+				a++;
+			} while (a <= 9);
+		}
+
+		System.out.println();
+		System.out.println("----------------------------------");
+		// 使用while迴圈+do while迴圈
+		System.out.println("使用while迴圈+do while迴圈製作九九乘法表:");
+		System.out.println();
+		int i = 1;
+		while (i <= 9) {
+			int a = 1;
+			do {
+				System.out.print(i + "*" + a + "=" + i * a + "\t");
+				a++;
+			} while (a <= 9);
+			System.out.println();
+			i++;
+		}
+	}
+}
